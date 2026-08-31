9a4cfdfcb14055c4a7327852b5a4aaa4967d0eac
Gabriel Santana <gabrielcs.df@gmail.com>
2026-08-31 19:01:04 -0300

    test: add content analysis integration test

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,4 +1,4 @@
-from iochunter import extrair_ipv4, extrair_sha256, extrair_cves
+from iochunter import extrair_ipv4, extrair_sha256, extrair_cves, analisar_conteudo
 
 def test_extrair_ipv4_valido():
     ipv4_validos, ipv4_invalidos = extrair_ipv4('192.168.0.112')
@@ -26,3 +26,19 @@ def test_extrair_cve_minusculo_normalizado():
     cves = extrair_cves('cve-2024-3094')
 
     assert cves == ['CVE-2024-3094']
+
+def test_analisar_conteudo():
+    resultados = analisar_conteudo("""
+                                    Connection from 192.168.1.10
+                                    Malformed address 256.300.12.1
+                                    Exploit targeting cve-2024-3094
+                                    Hash: ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
+                                """)
+    assert resultados == {
+    "ipv4": ["192.168.1.10"],
+    "ipv4_falsos_candidatos": ["256.300.12.1"],
+    "sha256": [
+        "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
+    ],
+    "cves": ["CVE-2024-3094"]
+}
