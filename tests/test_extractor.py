0b771aa7ad3788bd8a35a06f9e19374a25b115e0
Gabriel Santana <gabrielcs.df@gmail.com>
2026-08-31 18:33:49 -0300

    test: add extractor unit tests

---
+++ tests/test_extractor.py
@@ -0,0 +1,28 @@
+from iochunter import extrair_ipv4, extrair_sha256, extrair_cves
+
+def test_extrair_ipv4_valido():
+    ipv4_validos, ipv4_invalidos = extrair_ipv4('192.168.0.112')
+
+    assert ipv4_validos == ['192.168.0.112']
+    assert ipv4_invalidos == []
+
+def test_extrair_ipv4_invalido():
+    ipv4_validos, ipv4_invalidos = extrair_ipv4("256.300.12.1")
+
+    assert ipv4_validos == []
+    assert ipv4_invalidos == ["256.300.12.1"]
+
+def test_extrair_sha256_valido():
+    sha256_encontrados = extrair_sha256('ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad')
+
+    assert sha256_encontrados == ['ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad']
+
+def test_extrair_sha256_invalido():
+    sha256_encontrados = extrair_sha256('macarena')
+
+    assert sha256_encontrados == []
+
+def test_extrair_cve_minusculo_normalizado():
+    cves = extrair_cves('cve-2024-3094')
+
+    assert cves == ['CVE-2024-3094']
